2c550e212fbfb88ad737da80d5acec9404227e40
najwaaulia05 <aulianajwa703@gmail.com>
2025-07-11 11:11:04 +0700

    Update db.js

--- db.js
+++ db.js
@@ -3,10 +3,11 @@ const mysql = require('mysql2');
 
 // Setup koneksi database
 const pool = mysql.createPool({
-  host: 'localhost',  // Ganti dengan host DB yang sesuai
-  user: 'root',       // Ganti dengan user DB yang sesuai
-  password: '',       // Ganti dengan password DB yang sesuai
-  database: 'kulinerkitav2', // Ganti dengan nama database yang sesuai
+  host: 'shortline.proxy.rlwy.net',
+  user: 'root',
+  password: 'sYTiLDWEIhLaZVVMRyIclTVvaUmDaZmU',
+  database: 'railway',
+  port: 3306,
 });
 
 module.exports = pool.promise();  // Menyediakan koneksi pool dengan promise
